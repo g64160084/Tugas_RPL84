fc138034f670d5b638a88fd650b43b716c051da8
g64160084 <fjust212121@gmail.com>
2018-04-14 16:51:48 +0700

    Rpl-g684

---
+++ basic/views/siswa/index.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\SiswaSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Mahasiswa';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="Mahas-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Mahasiswa', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'nama:ntext',
+            'nim',
+            'alamat:ntext',
+            'no_hp',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
---
+++ basic/views/site/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Selamat Datang!</h1>
+
+        <p class="lead">Klik tombol dibawah untuk beralih ke page CRUD.</p>
+
+        <p><a class="btn btn-lg btn-success" href="/basic/web/index.php?r=siswa%2Findex">Ayo!</a></p>
+    </div>
+
+    <div class="body-content">
+
+        
+
+    </div>
+</div>
